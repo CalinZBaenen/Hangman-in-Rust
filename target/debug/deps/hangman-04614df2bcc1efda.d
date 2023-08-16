219e68cf9b989816c7b02f2f20f178b3c9c6ef0b
Calin Baenen <calinbaenen@gmail.com>
2023-08-16 10:12:47 -0500

    Removed the debug compilation.

--- target/debug/deps/hangman-04614df2bcc1efda.d
+++
@@ -1,7 +0,0 @@
-/home/calin/Projects/Hangman in Rust/target/debug/deps/hangman-04614df2bcc1efda.rmeta: src/main.rs src/game.rs src/load.rs
-
-/home/calin/Projects/Hangman in Rust/target/debug/deps/hangman-04614df2bcc1efda.d: src/main.rs src/game.rs src/load.rs
-
-src/main.rs:
-src/game.rs:
-src/load.rs:
